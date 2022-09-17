03a4fa36b30b5264419051f6c6d66b346d9679fc
DKhoi02 <khoidndgcc200058@fpt.edu.vn>
2022-09-17 09:05:25 +0700

    Day 2

---
+++ hello.js
@@ -0,0 +1,70 @@
+// Display Text data 
+// const http = require('http');
+// const hostname = 'localhost';
+// const port = 3000;
+// const server = http.createServer((req, res) => {
+// res.statusCode = 200;
+// res.setHeader('Content-Type', 'text/plain');
+// res.end('Hello World ');
+// });
+// server.listen(port, hostname, () => {
+// console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+// Display Json data 
+
+// const http = require("http");
+// const hostname = "localhost";
+// const port = 3000;
+// const server = http.createServer((req, res) => {
+// res.statusCode = 200;
+// res.setHeader("Content-Type", "application/json");
+// res.end('{ "message" : "Hello World " }');
+// });
+// server.listen(port, hostname, () => {
+// console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+//// Display HTML data 
+
+// const http = require("http");
+// const hostname = "localhost";
+// const port = 3000;
+// const server = http.createServer((req, res) => {
+// res.statusCode = 200;
+// res.setHeader("Content-Type", "text/html");
+// res.end(
+// "<h1>Hello World</h1><p>This is a HTML response</p><ol><li>One</li><li>Two</li><li>Three</li></ol>");
+// });
+// server.listen(port, hostname, () => {
+// console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+const http = require("http");
+const hostname = "localhost";
+const port = 3000;
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader("Content-Type", "text/html");
+switch (req.url) {
+case "/home":
+res.writeHead(200);
+res.end("<h1>This is Home page</h1>");
+break;
+case "/about":
+    http.rea
+res.writeHead(200);
+res.end("<h1>This is About page</h1>");
+break;
+default:
+    res.writeHead(200);
+res.end("<h1>This is Basic</h1>");
+break;
+}
+});
+server.listen(port, hostname, () => {
+console.log(`Server running at http://${hostname}:${port}/`);
+});
